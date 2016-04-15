f0f6d42e91bdf880fab4b2f25c37bb7748da68e4
Jacques Moati <jacques@moati.net>
2016-04-15 07:29:47 +0200

    add getDate, php-cs-fixer and php7

--- Service/ExifToolService.php
+++ Service/ExifToolService.php
@@ -6,14 +6,25 @@ use Symfony\Component\Process\Process;
 
 class ExifToolService
 {
+    /**
+     * @var string
+     */
     protected $exiftoolFile;
 
-    public function __construct($rootDir)
+    /**
+     * @param string $rootDir
+     */
+    public function __construct(string $rootDir)
     {
-        $this->exiftoolFile = realpath($rootDir . '/../vendor/exiftool/exiftool/exiftool');
+        $this->exiftoolFile = realpath($rootDir.'/../vendor/exiftool/exiftool/exiftool');
     }
 
-    public function file($filename)
+    /**
+     * @param string $filename
+     *
+     * @return array
+     */
+    public function file(string $filename) : array
     {
         $process = new Process(sprintf('perl %s -charset UTF-8 -g -j -fast -q "%s"', $this->exiftoolFile, $filename));
         $process->run();
@@ -27,6 +38,35 @@ class ExifToolService
         unset($data['File']['FileModifyDate']);
         unset($data['File']['FileAccessDate']);
         unset($data['File']['FileInodeChangeDate']);
+
+        return $data;
+    }
+
+    /**
+     * @param array $exif
+     *
+     * @return \DateTime|null
+     */
+    public function getDate(array $exif)
+    {
+        $date = null;
+
+        foreach ($exif as $data) {
+            if (isset($data['CreateDate'])) {
+                try {
+                    $date = new \DateTime($data['CreateDate']);
+                    $string = $date->format('Y-m-d H:i:s');
+
+                    if (preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}$/', $string)) {
+                        break;
+                    } else {
+                        $date = null;
+                    }
+                } catch (\Exception $e) {
+                    $date = null;
+                }
+            }
+        }
 
         return $data;
     }
